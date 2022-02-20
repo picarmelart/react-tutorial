f37ca5a8f642a50bd081571f8c481fcb6250d574
Carmela <78534931+picarmelart@users.noreply.github.com>
2022-02-20 18:05:13 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+ 
+
+function App({login}) {
+  const [data, setData]= useState(null);
+  const[loading, setLoading] =useState(false);
+  const[error, setError] = useState(null);
+  
+
+useEffect(() => {
+  if(!login) return;
+  setLoading(true);
+fetch(`https://api.github.com/users/picarmelart`)
+  .then((response) => response.json())
+  .then(setData)
+  .then(()=>setLoading(false))
+  .catch(setError);
+ }, [login]);
+
+ if(loading) return <h1>loading...</h1>;
+ if(error) return <pre>{JSON.stringify(error,null,2)}</pre>;
+ if(!data) return null;
+
+
+return (
+<div> 
+<h1>{data.name}</h1>
+<p>{data.location}</p>
+<img alt={data.login} src={data.avatar_url} />
+</div>
+);
+}
+
+
+
+export default App;
